3c5b314bab39d034435da3540dc730ca08b85ba7
Ilana <ilanaenderman@hotmail.com>
2016-11-23 12:04:05 +0100

    started with ajax in custom.js to create a profile

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,8 +1,17 @@
 //trigger create profile modal
 $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('#modal2').modal()
-  })
+    $('#modal2').modal(function() {
+    	$('#submitProfile').click(function() {
+    		var profile = {
+    			input: $("#submitProfile").val()
+    		}
+    		$.get("/createdProfile", profile, function(response) {
+    			$('#error-message').html(message) //hier waren we gebleven!!!
+    		})
+    	})
+    })
+})
 
 //Keep create profile modal open after submit
 
@@ -19,3 +28,23 @@ $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('#modal1').modal()
   })
+
+
+//Ajax create profile
+$(document).ready( function () {
+	$("#provider-json").keyup( () => {
+		if( timer() ){
+
+		var letter = { 
+			input: $("#provider-json").val()
+		}
+		$.post("/searchAutocomplete", letter, (response) => {
+				$('#result').html("")
+				for( var i = 0; i < response.data.length; i++) {
+					$("#result").append(response.data[i].firstName + " " + response.data[i].lastName + ": " + response.data[i].email + "<br>")
+				}
+			})
+		}
+	} else {console.log("false")}
+	})
+})
