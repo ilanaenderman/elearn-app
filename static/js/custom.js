62ea43ec56ac02b24da6cfb3629a378c22081a36
Ilana <ilanaenderman@hotmail.com>
2016-11-23 12:09:43 +0100

    test

--- static/js/custom.js
+++ static/js/custom.js
@@ -14,7 +14,6 @@ $(document).ready(function(){
 })
 
 //Keep create profile modal open after submit
-
 $(document).ready(function(){
 	var url = window.location.href
 	if(url.indexOf('?message=') != -1 || url.indexOf('/?message=') != -1) {
@@ -28,7 +27,6 @@ $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('#modal1').modal()
   })
-
 
 //Ajax create profile
 $(document).ready( function () {
