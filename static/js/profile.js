9800a08dfc4de7159242dc94a04778f10786b8f6
LauraXiulan <laurakramer91@gmail.com>
2016-11-25 14:23:15 +0100

    Changed size navbar

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,22 +1,5 @@
 $(document).ready(function() {
 	$("#gs-closebut").click(function() {
 		$("#gs-closebut").attr('action', location.pathname)
-		// if(location.pathname.substring(1, location.pathname.length) == 'memory-food-nl') {
-		
-			// $("#food-nl").css({image: url("images/food-btn-color")})
-		// } else if (location.pathname.substring(1, location.pathname.length) == 'memory-animal-nl') {
-		// 	$("#family-nl").attr("href", "/memory-family-nl")
-			// $("#animal-nl").attr({image: url("images/animal-btn-color")})
-		// }
 	})
-	
-
-
-	// 	$("#animal-nl").attr("href", location)
-	// 	// $("#food-nl").css({image: url("images/food-btn-color")})
-	// })
-	
-	// $("#family-nl").removeAttr('href')
-	//If animal-nl = finished
-	//$("#family-nl").attr("href", location)
 })
