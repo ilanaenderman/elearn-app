e5c5bf666f53fb6635f2ce32cd054679f509740b
Ilana <ilanaenderman@hotmail.com>
2016-11-25 14:23:34 +0100

    tried my best but didn't succeed with ajax in modals

--- static/js/custom.js
+++ static/js/custom.js
@@ -4,46 +4,18 @@ $(document).ready(function(){
     $('.modal').modal()
 })
 
-// $(document).ready(function() {
-//     $('#submitLogin').click(function() {
-// 		var profile = {
-// 			email: 	$("#email2").val(),
-// 			password: $("#password2").val(),
-// 		}
+$(document).ready(function() {
+    $('#submitLogin').click(function() {
+		var profile = {
+			email: 	$("#email2").val(),
+			password: $("#password2").val(),
+		}
 
-//         if(email.length === 0 ) {
-//             $("#error-message2").html("Please fill out email.")
-//         }
-
-//         if(password.length === 0 ) {
-//             $("#error-message2").html("Please fill out your password")
-//         }
-
-//         else{
-//             $.post("/loginProfile", profile, function(response) {
-//                 $("#error-message2").append(response.message)
-//             })
-
-//         }
-//     })
-// })
-
-// going to try a different way
-if (loginUsername.length != 0) {
-  console.log('there is a login: ' + loginUsername);
-    // make an ajax call
-    $.ajax({
-    dataType: 'json',
-    data: AjaxLoginData,
-    type: 'post',
-      url:"http://localhost:4200/api/v1/users",
-      success: (data, textStatus, jqXHR) ->
-          if typeof data.redirect == 'string'
-              window.location = data.redirect
-      success: refreshStories,
-      error: foundError
-    });
-// ? try above 
+        $.post("/loginProfile", profile, function(response) {
+            $("#error-message2").append(response.message)
+        })
+    })
+})
 
 
 //Keep create profile modal open after submit
