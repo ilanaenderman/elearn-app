f4c6b6f281f4dd1a19d1efdb586990ed0df95fa2
Ilana <ilanaenderman@hotmail.com>
2017-02-07 12:54:13 +0100

    changed update settings.js

--- routes/settings.js
+++ routes/settings.js
@@ -12,135 +12,160 @@ router.get('/settings', (request, response) => {
 })
 
 router.post('/settings', (request, response) => {
-	let user 		= request.session.user
-	let newUserName = request.body.newUserName
-	let newEmail 	= request.body.newEmail
-	let newFullName = request.body.newFullName
-	let fCharName	= newFullName.substr(0,1)
-	let fCharUser	= newUserName.substr(0,1)
+	let filter 	   = {}
+	let attributes = ['id']
+	let user 	   = request.session.user
 
-	if( !fCharName.match(/[a-zA-Z ]/)) {
-		response.redirect('/settings?message=' + encodeURIComponent("First letter of full name must be alphabetic."))
-		return
-	}
+	if(request.body.newUserName) (filter.userName = request.body.newUserName) && (attributes.push('userName'))
+	if(request.body.newEmail) (filter.email = request.body.newEmail) && (attributes.push('email'))
+	if(request.body.newFullName) (filter.fullName = request.body.newFullName) && (attributes.push('fullName'))
 
-	if( !fCharUser.match(/[a-zA-Z ]/)) {
-		response.redirect('/settings?message=' + encodeURIComponent("First letter of username must be alphabetic."))
-		return
-	}
-
-	if(  newUserName != "" && newEmail != "" && newFullName!= "") {
-		db.user.findOne({
-			where: {id: user.id},
-			attributes: ['id', 'userName', 'email', 'fullName']
-		}).then( newSetting => {
-			newSetting.update({
-				userName: newUserName,
-				email: newEmail,
-				fullName: newFullName
-			}).then( newSetting => {
-				response.redirect('/settings?message=' + encodeURIComponent("Succesfully changed your username and email address. Next time you login the changes will be visible."))
-			})
-		})
-	}
-
-	else if( newEmail != "") {
-		db.user.findOne({
-			where: {id: user.id},
-			attributes: ['id', 'email']
-		}).then( newSetting => {
-			newSetting.update({
-				email: newEmail
-			}).then( newSetting => {
-				response.redirect('/settings?message=' + encodeURIComponent("Succesfully changed your email address. Next time you login the changes will be visible"))
-			})
-		})
-	}
-
-	else if( newEmail != "" && newUserName != "") {
-		db.user.findOne({
-			where: {id: user.id},
-			attributes: ['id', 'email', 'userName']
-		}).then( newSetting => {
-			newSetting.update({
-				userName: newUserName,
-				email: newEmail
-			}).then( newSetting => {
-				response.redirect('/settings?message=' + encodeURIComponent("Succesfully changed your username and email address. Next time you login the changes will be visible"))
-			})
-		})
-	}
-
-	else if( newEmail != "" && newFullName != "") {
-		db.user.findOne({
-			where: {id: user.id},
-			attributes: ['id', 'email', 'fullName']
-		}).then( newSetting => {
-			newSetting.update({
-				fullName: newFullName,
-				email: newEmail
-			}).then( newSetting => {
-				response.redirect('/settings?message=' + encodeURIComponent("Succesfully changed your full name and email address. Next time you login the changes will be visible"))
-			})
-		})
-	}
-	else if( newFullName != "" && newUserName != "") {
-		if( newUserName.length <= 2 || newUserName.length >= 13){
-		response.redirect('/settings?message=' + encodeURIComponent("Username must be between 2 and 13 characters long."))
-		}
-		else(
+	db.user.findOne({
+		where: {id: user.id},
+		attributes: attributes
+	}).then( updateFilter => {
+		updateFilter.update(filter
+		).then(updateUser => {
 			db.user.findOne({
-				where: {id: user.id},
-				attributes: ['id', 'fullName', 'userName']
-			}).then( newSetting => {
-				newSetting.update({
-					userName: newUserName,
-					fullName: newFullName
-				}).then( newSetting => {
-					response.redirect('/settings?message=' + encodeURIComponent("Succesfully changed your username and full name. Next time you login the changes will be visible"))
-				})
-			})
-		)
-	}
-
-	else if( newUserName != "") {
-		if( newUserName.length <= 2 || newUserName.length >= 13){
-		response.redirect('/settings?message=' + encodeURIComponent("Username must be between 2 and 13 characters long."))
-		}
-
-		else(
-			db.user.findOne({
-				where: {id: user.id},
-				attributes: ['id', 'userName']
-			}).then( newSetting => {
-				console.log(newSetting)
-				newSetting.update({
-					userName: newUserName
-				}).then( newSetting => {
-					response.redirect('/settings?message=' + encodeURIComponent("Succesfully changed your settings. Next time you login the changes will be visible."))
-				})
-			})
-		)
-	}
-
-	else if( newFullName != "") {
-		db.user.findOne({
-			where: {id: user.id},
-			attributes: ['id', 'fullName']
-		}).then( newSetting => {
-			console.log(newSetting)
-			newSetting.update({
-				fullName: newFullName
-			}).then( newSetting => {
-				response.redirect('/settings?message=' + encodeURIComponent("Succesfully changed your full name. Next time you login the changes will be visible."))
+				where: {id: user.id}
+			}).then( updateUser => {
+				console.log(updateUser)
+				response.render('settings', {message: "Succesfully updated your settings.", user: updateUser})
 			})
 		})
-	}
-	else {
-		console.log('none')
-		response.redirect('/settings?message=' + encodeURIComponent("Please fill in a new full name, username and/or email address."))
-	}
+	})
 })
+
+// router.post('/settings', (request, response) => {
+// 	let user 		= request.session.user
+// 	let newUserName = request.body.newUserName
+// 	let newEmail 	= request.body.newEmail
+// 	let newFullName = request.body.newFullName
+// 	let fCharName	= newFullName.substr(0,1)
+// 	let fCharUser	= newUserName.substr(0,1)
+
+// 	if( !fCharName.match(/[a-zA-Z ]/)) {
+// 		response.redirect('/settings?message=' + encodeURIComponent("First letter of full name must be alphabetic."))
+// 		return
+// 	}
+
+// 	if( !fCharUser.match(/[a-zA-Z ]/)) {
+// 		response.redirect('/settings?message=' + encodeURIComponent("First letter of username must be alphabetic."))
+// 		return
+// 	}
+
+// 	if(  newUserName != "" && newEmail != "" && newFullName!= "") {
+// 		db.user.findOne({
+// 			where: {id: user.id},
+// 			attributes: ['id', 'userName', 'email', 'fullName']
+// 		}).then( newSetting => {
+// 			newSetting.update({
+// 				userName: newUserName,
+// 				email: newEmail,
+// 				fullName: newFullName
+// 			}).then( newSetting => {
+// 				response.redirect('/settings?message=' + encodeURIComponent("Succesfully changed your username and email address. Next time you login the changes will be visible."))
+// 			})
+// 		})
+// 	}
+
+// 	else if( newEmail != "") {
+// 		db.user.findOne({
+// 			where: {id: user.id},
+// 			attributes: ['id', 'email']
+// 		}).then( newSetting => {
+// 			newSetting.update({
+// 				email: newEmail
+// 			}).then( newSetting => {
+// 				response.redirect('/settings?message=' + encodeURIComponent("Succesfully changed your email address. Next time you login the changes will be visible"))
+// 			})
+// 		})
+// 	}
+
+// 	else if( newEmail != "" && newUserName != "") {
+// 		db.user.findOne({
+// 			where: {id: user.id},
+// 			attributes: ['id', 'email', 'userName']
+// 		}).then( newSetting => {
+// 			newSetting.update({
+// 				userName: newUserName,
+// 				email: newEmail
+// 			}).then( newSetting => {
+// 				response.redirect('/settings?message=' + encodeURIComponent("Succesfully changed your username and email address. Next time you login the changes will be visible"))
+// 			})
+// 		})
+// 	}
+
+// 	else if( newEmail != "" && newFullName != "") {
+// 		db.user.findOne({
+// 			where: {id: user.id},
+// 			attributes: ['id', 'email', 'fullName']
+// 		}).then( newSetting => {
+// 			newSetting.update({
+// 				fullName: newFullName,
+// 				email: newEmail
+// 			}).then( newSetting => {
+// 				response.redirect('/settings?message=' + encodeURIComponent("Succesfully changed your full name and email address. Next time you login the changes will be visible"))
+// 			})
+// 		})
+// 	}
+// 	else if( newFullName != "" && newUserName != "") {
+// 		if( newUserName.length <= 2 || newUserName.length >= 13){
+// 		response.redirect('/settings?message=' + encodeURIComponent("Username must be between 2 and 13 characters long."))
+// 		}
+// 		else(
+// 			db.user.findOne({
+// 				where: {id: user.id},
+// 				attributes: ['id', 'fullName', 'userName']
+// 			}).then( newSetting => {
+// 				newSetting.update({
+// 					userName: newUserName,
+// 					fullName: newFullName
+// 				}).then( newSetting => {
+// 					response.redirect('/settings?message=' + encodeURIComponent("Succesfully changed your username and full name. Next time you login the changes will be visible"))
+// 				})
+// 			})
+// 		)
+// 	}
+
+// 	else if( newUserName != "") {
+// 		if( newUserName.length <= 2 || newUserName.length >= 13){
+// 		response.redirect('/settings?message=' + encodeURIComponent("Username must be between 2 and 13 characters long."))
+// 		}
+
+// 		else(
+// 			db.user.findOne({
+// 				where: {id: user.id},
+// 				attributes: ['id', 'userName']
+// 			}).then( newSetting => {
+// 				console.log(newSetting)
+// 				newSetting.update({
+// 					userName: newUserName
+// 				}).then( newSetting => {
+// 					response.redirect('/settings?message=' + encodeURIComponent("Succesfully changed your settings. Next time you login the changes will be visible."))
+// 				})
+// 			})
+// 		)
+// 	}
+
+// 	else if( newFullName != "") {
+// 		db.user.findOne({
+// 			where: {id: user.id},
+// 			attributes: ['id', 'fullName']
+// 		}).then( newSetting => {
+// 			console.log(newSetting)
+// 			newSetting.update({
+// 				fullName: newFullName
+// 			}).then( newSetting => {
+// 				response.redirect('/settings?message=' + encodeURIComponent("Succesfully changed your full name. Next time you login the changes will be visible."))
+// 			})
+// 		})
+// 	}
+// 	else {
+// 		console.log('none')
+// 		response.redirect('/settings?message=' + encodeURIComponent("Please fill in a new full name, username and/or email address."))
+// 	}
+// })
 
 
 
